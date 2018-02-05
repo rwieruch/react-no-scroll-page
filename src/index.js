c72ff0df7462261cd4dcf786211a77056c91507f
Robin Wieruch <wrobin@gmx.net>
2018-02-05 10:20:18 +0100

    include array fill from core-js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'core-js/fn/array/from';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
